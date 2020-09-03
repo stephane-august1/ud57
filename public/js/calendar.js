f0cd4b19fdcf39158b9e6cd7f942421701ff1fea
@ugust1 <stephane.august1@gmail.com>
2020-09-03 17:31:16 +0200

    add and modif

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,33 +1,106 @@
-console.log('test',test)
+console.log('test', test[0].description)
+
+ 
 document.addEventListener('DOMContentLoaded', function () {
-       
+      
+ 
   var calendarEl = document.getElementById('#calendrier');
   
   var calendar = new FullCalendar.Calendar(calendarEl, {
+    
     initialView: 'dayGridMonth',
     locale: 'fr',
+    buttonText: {
+      today: 'Aujourd\'hui',
+      month: 'Mois',
+      week: 'Semaine',
+      day: 'Jour',
+      list: 'Liste'
+    },
     timeZone: 'Europe/Paris',
     headerToolbar: {
       start: 'prev,next today',
       center: 'title',
-        end: 'dayGridMonth,timeGridWeek,timeGridDay',
+      end: 'dayGridMonth,timeGridWeek,timeGridDay,list',
+  
       
+    },
       
-      },
-      
-    dayMaxEventRows : 4,
-    dayMaxEvents : true,
-    dayPopoverFormat: {month: 'long', day: 'numeric', year: 'numeric'},
-   
-    events:  test,
-   
+    dayMaxEventRows: 4,
+    dayMaxEvents: true,
+    dayPopoverFormat: { month: 'long', day: 'numeric', year: 'numeric' },
+ 
+    events: test,
+  
+    
             
     editable: true,
     eventResizableFromStart: true,
-  
+   
 
   });
-   calendar.on('eventChange',(e) =>{
+  //recuperer la liste d"events
+  /*
+  var tet = calendar.getEvents();
+  console.log('tet', tet);
+  for (let i = 0; i < tet.length; i++) {
+    const element = tet[i];
+    
+  console.log('titre : ', tet[i].title);
+  console.log('description: ', tet[i]._def.extendedProps.description);
+}*/
+  
+  calendar.on('eventMouseEnter', (e, ev) => {
+  //on récupère le id de l event
+      // console.log('id', e.event.id)
+      var id = e.event.id;
+  //on récupère les données de  l event avec le id
+      var tet = calendar.getEventById(id);
+        console.log('tet', tet);
+    
+  console.log('titre : ', tet.title);
+  console.log('description: ', tet._def.extendedProps.description);
+
+   //transformation  de la date
+      function format(d) {
+          //récuperation de l'heure
+           var hours = d.getHours();
+           //mise à l'heure francaise -Heures
+           hours = (hours - 2);
+           var minutes = d.getMinutes();
+           if (minutes === 0) {
+             minutes = minutes + '0';
+             var time = hours + ':' + minutes;
+           }
+           else {
+             minutes = minutes
+           
+           var time = hours + ':' + minutes;
+         }
+      // console.log('time',time);
+    return (time); 
+           } (new Date());
+           const mydatestart = new Date(e.event.start);
+           const mydatestop = new Date(e.event.end);
+           my_time_formatstart = format(mydatestart);
+            my_time_formatstop = format(mydatestop);
+        
+   // console.log('desc', e.event._def.extendedProps.description);
+   
+  //recupération des infos
+    $('#tooltip').html('De '+my_time_formatstart+' à '+my_time_formatstop+ '<br><span style="color:rgb(90, 18, 18);"> à '+e.event.title+'</span><br><span style="color:rgb(255, 60, 60);"> '+e.event._def.extendedProps.description+'</span>');
+    
+    $('#tooltip').css({"display": "inline-block","margin-left":"40%"} );
+
+  })
+
+   calendar.on('eventMouseLeave', (e, ev) => {
+    //enleve le tooltip 
+    $('#tooltip').css('display','none');
+
+  })
+ 
+   calendar.on('eventChange',(e,view) =>{
         console.log(e);
           console.log('2emlog',e.event._def.extendedProps.description);
        let url = `/calendar/${e.event.id}/editevent`
@@ -38,11 +111,12 @@ document.addEventListener('DOMContentLoaded', function () {
             "start":e.event.start,
             "fin":e.event.end,
             "textColor": e.event._def.ui.textColor,
-            "backgroundColor": e.event._def.ui.backgroundColor,
+            "backgroundColor": e.event.backgroundColor,
             "borderColor": e.event._def.ui.borderColor,
             "allDay":e.event.allDay
          
        }
+     
        let xhr = new XMLHttpRequest
 
        xhr.open("PUT",url)
@@ -51,19 +125,50 @@ document.addEventListener('DOMContentLoaded', function () {
     }),
    //event au click ouverture modal
        calendar.on('eventClick',(ev, jsEvent, view) => {
-      
+       $('#tooltip').css('display','none');
            var eventobj = ev;
-    
-           //console.log(ev);
+           
+   
+           
+
+         function format(d) {
+           dd = d.toUTCString();
+           var month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+           var date = d.getDate() + " " + month[d.getMonth()] + ", " + d.getFullYear();
+         //  var time = d.toTimeString().toLowerCase();
+           // var newt = d;
+           var hours = d.getHours();
+           //mise a l'heure francaise -Heures
+           hours = (hours - 2);
+           var minutes = d.getMinutes();
+           if (minutes === 0) {
+             minutes = minutes + '0';
+             var time = hours + ':' + minutes;
+           }
+           else {
+             minutes = minutes
+           
+           var time = hours + ':' + minutes;
+         }
+       console.log('time',time);
+    return (date + " à " + time); 
+           } (new Date());
+           const mydatestart = new Date(ev.event.start);
+           const mydatestop = new Date(ev.event.end);
+           my_date_formatstart = format(mydatestart);
+            my_date_formatstop = format(mydatestop);
+           console.log(my_date_formatstart);
+           console.log(my_date_formatstop);
            jQuery.noConflict(); 
            let url = `/calendar/${ev.event.id}/edit`;
            
         $('#hrefid').attr('href',url);
         $('#modalTitle').html(ev.event.title);
            $('#modalBody').html(ev.event._def.extendedProps.description);
-           $("#start").html(ev.event.start)
-            $("#end").html(ev.event.end)
-        $('#idedit').html(ev.event.id);
+           $("#start").html(my_date_formatstart)
+            $("#end").html(my_date_formatstop)
+         $('#idedit').html(ev.event.id);
+        
         $('#exampleModal').modal('show')
         
         
--- public/js/map.js
+++ public/js/map.js
@@ -1,3 +1,63 @@
+
+
+//recupere le champ ville
+ const champVille = document.getElementById('champ-ville')
+console.log(champVille);
+// On écoute l'évènement "change" sur le champ ville
+champVille.addEventListener("change", function () {
+    let champVille2 = $('#champ-ville').val();
+   
+   
+    // Ici nous chercherons les coordonnées GPS de la ville saisie
+    ajaxGet(`https://nominatim.openstreetmap.org/search?q=${this.value}&format=json&addressdetails=1&limit=1&polygon_svg=1`).then(reponse => {
+        // On convertit la réponse en objet Javascript
+        let data = JSON.parse(reponse)
+        console.log('data: ', data);
+        // On stocke la latitude et la longitude dans la variable ville
+        var lon = data[0].lon;
+        var lat = data[0].lat;
+       //'[49.2207124,6.9240547]'
+        ville = ['[' + lat + ', ' + lon + '],',];
+        $("#lon").html(data[0].lon);
+        $("#lat").html( data[0].lat);
+        //on fait un tableau de marker:
+        mesmarker = [];
+      
+        mesmarker = mesmarker.push(data[0].lat+data[0].lon);
+        console.log('ville', ville[0]);
+        console.log('ville', mesmarker);
+        //on affiche le nouveau marker
+        //var layer = L.marker([49.1835376,6.874619]).addTo(map);
+        for (let i = 0; i < ville.length; i++) {
+            //modifier le marker avec une image
+var myIcon = L.icon({
+    iconUrl: 'goutterigolo.png',
+    iconSize: [48, 65],
+    iconAnchor: [22, 4],
+    popupAnchor: [-3, -76],
+    //shadowUrl: 'goutterigolo.png',
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+});
+        
+        var layer = L.marker([data[0].lat, data[0].lon], {
+    draggable:true,
+    icon: myIcon,
+}).addTo(map);
+        layer.addTo(map);
+    }
+//layer.remove();
+
+  
+       
+          
+    })
+   
+})
+   
+
+     
+
 var map = L.map('map').setView([49.1191, 6.1727], 8);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -8,9 +68,12 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoic3RlcGhhbmVhdWd1c3QxIiwiYSI6ImNrZWltMnZ1bDBhazIyeHBpdGt3b2VuZGQifQ.ROjzrcVm5OlR6YgjB3nJgg'
 }).addTo(map);
+   
 
+
+   
 //modifier le marker avec une image
-/*var myIcon = L.icon({
+var myIcon = L.icon({
     iconUrl: 'goutterigolo.png',
     iconSize: [48, 65],
     iconAnchor: [22, 94],
@@ -18,12 +81,27 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     //shadowUrl: 'goutterigolo.png',
     shadowSize: [68, 95],
     shadowAnchor: [22, 94]
-});*/
+});
 //simple marker
-var marker = L.marker([49.1191, 6.1727],{
+/*
+var tab = [
+    [49.1191, 6.1727],
+    [49.0991, 6.0727],
+    [48.9991, 6.0927],
+    [48.811, 6.1700],
+
+]
+for (let i = 0; i < tab.length; i++) {
+  var marker = L.marker(
+    tab[i],
+    {
     draggable:true,
-    //icon: myIcon,
+    icon: myIcon,
 }).addTo(map);
+    
+}*/
+var marker = L.marker(
+    [49.1191, 6.1727]).addTo(map);
 //marker en rond
 var circle = L.circle([48.708, 5.00], {
     color: 'red',
@@ -34,7 +112,7 @@ var circle = L.circle([48.708, 5.00], {
 //popup au clic
 circle.bindPopup("<a href='#'>my tooltip text</a>");
 //popup mousehover
-marker.bindTooltip("<a href='#'>my tooltip text</a>").openTooltip();
+//marker.bindTooltip("<a href='#'>my tooltip text</a>").openTooltip();
 
 
 
